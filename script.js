b7c35cd6d1e3fa7eccb6bd81925ecfe741ab7404
Wimanjaya16 <wimojs25@gmail.com>
2024-07-19 10:37:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Tambahkan interaktivitas dengan JavaScript di sini jika diperlukan
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulir telah dikirim!');
+});
